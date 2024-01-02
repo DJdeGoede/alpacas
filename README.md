f3935b0e08487d419d32862caf44557351db07ea
Dick de Goede <djdegoed@gmail.com>
2024-01-02 08:57:10 +0100

    adding some info and first py file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # alpacas
+This is the alpaca app containing Python packages and modules for testing purposes.
---
+++ alpacas.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def main():
+    print(f"Welcome to the alpacas app!")
+
+
+if __name__ == "__main__":
+    main()
